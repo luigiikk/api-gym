0544ff4e84aa19a7eedb91d539f022e5c771257e
luigiikk <luigimee15@gmail.com>
2025-02-27 23:34:04 -0300

    🧪test: feat: add Check-in use case test

---
+++ src/use-cases/check-ins.spec.ts
@@ -0,0 +1,21 @@
+import { beforeEach, describe, expect, it } from 'vitest'
+import { InMemoryCheckInsRepository } from '../repositories/in-memory/in-memory-check-ins-repository'
+import { CheckInUseCase } from './checkin'
+import { ResourceNotFoundError } from './errors/resource-not-found-error'
+
+let checkInsrespository: InMemoryCheckInsRepository
+let checkInUseCase: CheckInUseCase
+describe('Get User Profile Use Case', () => {
+  beforeEach(() => {
+    checkInsrespository = new InMemoryCheckInsRepository()
+    checkInUseCase = new CheckInUseCase(checkInsrespository)
+  })
+  it('should be able to check in', async () => {
+    const { checkIn } = await checkInUseCase.execute({
+      gymId: 'gym-01',
+      userId: 'user-01',
+    })
+
+    expect(checkIn.id).toEqual(expect.any(String))
+  })
+})
