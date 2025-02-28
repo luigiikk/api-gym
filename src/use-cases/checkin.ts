45f1292009539693eaa557ee343c610f3ba945f3
luigiikk <luigimee15@gmail.com>
2025-02-27 23:33:51 -0300

    ✨feat: feat: add Check-in use case

---
+++ src/use-cases/checkin.ts
@@ -0,0 +1,29 @@
+import type { CheckIn, User } from '@prisma/client'
+import type { CheckInsRepository } from '../repositories/check-in-repository'
+
+interface CheckInUseCaseParams {
+  userId: string
+  gymId: string
+}
+
+interface CheckInUseCaseResponse {
+  checkIn: CheckIn
+}
+
+export class CheckInUseCase {
+  constructor(private checkInsRepository: CheckInsRepository) {}
+
+  async execute({
+    userId,
+    gymId,
+  }: CheckInUseCaseParams): Promise<CheckInUseCaseResponse> {
+    const checkIn = await this.checkInsRepository.create({
+      user_id: userId,
+      gym_id: gymId,
+    })
+
+    return {
+      checkIn,
+    }
+  }
+}
