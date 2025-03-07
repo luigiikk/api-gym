0c5b7224146068fb4f8a34bc56ea4b6660cb64df
luigikk <luigimee15@gmail.com>
2025-03-07 17:18:57 -0300

    🧪test: add pagination test for check-in history use case

--- src/use-cases/fetch-user-check-ins-history.spec.ts
+++ src/use-cases/fetch-user-check-ins-history.spec.ts
@@ -7,8 +7,9 @@ let fetchUserCheckinsHistoryUseCase: FetchUserCheckInsHistoryUseCase
 describe('Fetch User Check-ins History Use Case', () => {
   beforeEach(async () => {
     checkInsrespository = new InMemoryCheckInsRepository()
-    fetchUserCheckinsHistoryUseCase = new FetchUserCheckInsHistoryUseCase(checkInsrespository)
-
+    fetchUserCheckinsHistoryUseCase = new FetchUserCheckInsHistoryUseCase(
+      checkInsrespository
+    )
   })
   it('should be able to fetch user check-in history', async () => {
     await checkInsrespository.create({
@@ -21,12 +22,31 @@ describe('Fetch User Check-ins History Use Case', () => {
     })
     const { checkIns } = await fetchUserCheckinsHistoryUseCase.execute({
       userId: 'user-01',
+      page: 1,
     })
 
     expect(checkIns).toHaveLength(2)
     expect(checkIns).toEqual([
-      expect.objectContaining({gym_id: 'gym-01'}),
-      expect.objectContaining({gym_id: 'gym-02'})
+      expect.objectContaining({ gym_id: 'gym-01' }),
+      expect.objectContaining({ gym_id: 'gym-02' }),
+    ])
+  })
+  it('should be able to fetch paginated user check-in history', async () => {
+    for (let i = 1; i <= 22; i++) {
+      await checkInsrespository.create({
+        gym_id: `gym-${i}`,
+        user_id: 'user-01',
+      })
+    }
+    const { checkIns } = await fetchUserCheckinsHistoryUseCase.execute({
+      userId: 'user-01',
+      page: 2,
+    })
+
+    expect(checkIns).toHaveLength(2)
+    expect(checkIns).toEqual([
+      expect.objectContaining({ gym_id: 'gym-21' }),
+      expect.objectContaining({ gym_id: 'gym-22' }),
     ])
   })
 })
