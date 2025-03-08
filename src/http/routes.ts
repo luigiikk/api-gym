e3319a22c23588582f1af02bc797e4f5dc9f0779
luigikk <luigimee15@gmail.com>
2025-03-08 12:01:59 -0300

    ✨feat: implement JWT authentication

--- src/http/routes.ts
+++ src/http/routes.ts
@@ -1,8 +1,11 @@
 import type { FastifyInstance } from 'fastify'
 import { authenticate } from './controllers/authenticate'
 import { register } from './controllers/register'
+import { profile } from './controllers/profile'
 
 export async function appRoutes(app: FastifyInstance) {
   app.post('/users', register)
   app.post('/sessions', authenticate)
+
+  app.get('/me', profile)
 }
