a134d1d5122f17755e7ce6bbe78b9d0072f11a51
luigikk <luigimee15@gmail.com>
2025-03-07 21:43:11 -0300

    ✨feat: add findById method in Prisma users repository

--- src/repositories/prisma/prisma-users-repository.ts
+++ src/repositories/prisma/prisma-users-repository.ts
@@ -1,10 +1,15 @@
-import type { Prisma, User } from '@prisma/client'
+import type { Prisma } from '@prisma/client'
 import { prisma } from '../../lib/prisma'
 import type { UsersRepository } from '../users-repository'
 
 export class PrismaUsersReposity implements UsersRepository {
-  async findById(userId: string): Promise<User | null> {
-    
+  async findById(id: string) {
+    const user = await prisma.user.findUnique({
+      where: {
+        id,
+      },
+    })
+    return user
   }
   async create(data: Prisma.UserCreateInput) {
     const user = await prisma.user.create({
