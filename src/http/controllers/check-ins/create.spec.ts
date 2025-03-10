37d4de772a345c4aa2abb12610d180ad024fe060
luigikk <luigimee15@gmail.com>
2025-03-10 17:57:26 -0300

    🧪test: add create check-in  test

---
+++ src/http/controllers/check-ins/create.spec.ts
@@ -0,0 +1,37 @@
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it, test } from 'vitest'
+import { app } from '../../../app'
+import { CreateAndAuthenticateUser } from '../../../utils/test/create-and-authenticate-user'
+import { prisma } from '../../../lib/prisma'
+
+describe('Create Check-in Controller (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should be able to create a check-in', async () => {
+    const { token } = await CreateAndAuthenticateUser(app)
+
+    const gym = await prisma.gym.create({
+      data: {
+        title: 'gym',
+        description: null,
+        phone: null,
+        latitude: 12.7429572,
+        longitude: -38.2112697,
+      }
+    })
+    const response = await request(app.server)
+      .post(`/gyms/${gym.id}/check-ins`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({
+        latitude: 12.7429572,
+        longitude: -38.2112697,
+      })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
