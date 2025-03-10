20c92169fbb710d979231f48be2978af645724fd
luigikk <luigimee15@gmail.com>
2025-03-10 14:45:33 -0300

    🔧chore: separate database for end-to-end testing

--- prisma/vitest-environment-prisma/prisma-test-environment.ts
+++ prisma/vitest-environment-prisma/prisma-test-environment.ts
@@ -1,15 +1,38 @@
+import { randomUUID } from 'node:crypto'
+import 'dotenv/config'
+import { execSync } from 'node:child_process'
+import { PrismaClient } from '@prisma/client'
 import type { Environment } from 'vitest/environments'
 
-export default <Environment>{
+const prisma = new PrismaClient()
+
+function generateDataBaseURL(schema: string) {
+  if (!process.env.DATABASE_URL) {
+    throw new Error('Please provide a DATABASE_URL enviroonment variable')
+  }
+
+  const url = new URL(process.env.DATABASE_URL)
+  url.searchParams.set('schema', schema)
+
+  return url.toString()
+}
+export default (<Environment>{
   name: 'prisma',
   transformMode: 'ssr',
   async setup() {
-    console.log('Setup')
+    const schema = randomUUID()
+    const databaseURL = generateDataBaseURL(schema)
 
+    process.env.DATABASE_URL = databaseURL
+
+    execSync('npx prisma migrate deploy')
     return {
       async teardown() {
-        console.log('Teardwon')
+        await prisma.$executeRawUnsafe(
+          `DROP SCHEMA IF EXISTS "${schema}" CASCADE`
+        )
+        await prisma.$disconnect()
       },
     }
   },
-}
+})
