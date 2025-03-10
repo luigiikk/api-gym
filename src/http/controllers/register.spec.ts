156d30a43944de08b5b6e59d27e203d5b2019470
luigikk <luigimee15@gmail.com>
2025-03-10 15:04:09 -0300

    🧪test: add end-to-end test for register controller

---
+++ src/http/controllers/register.spec.ts
@@ -0,0 +1,22 @@
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it, test } from 'vitest'
+import { app } from '../../app'
+
+describe('Register Controller (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should be able to register', async () => {
+    const response = await request(app.server).post('/users').send({
+      name: 'Jhon Doe',
+      email: 'jhondoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(201)
+  })
+})
