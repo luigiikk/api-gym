710342688829cd9a49187043543127a6ec377cc8
luigikk <luigimee15@gmail.com>
2025-03-10 15:12:34 -0300

    🧪test: add end-to-end test for authentication controller

---
+++ src/http/controllers/authenticate.spec.ts
@@ -0,0 +1,29 @@
+import request from 'supertest'
+import { afterAll, beforeAll, describe, expect, it, test } from 'vitest'
+import { app } from '../../app'
+
+describe('Authenticate Controller (e2e)', () => {
+  beforeAll(async () => {
+    await app.ready()
+  })
+
+  afterAll(async () => {
+    await app.close()
+  })
+  it('should be able to Authenticate', async () => {
+    await request(app.server).post('/users').send({
+      name: 'Jhon Doe',
+      email: 'jhondoe@example.com',
+      password: '123456',
+    })
+    const response = await request(app.server).post('/sessions').send({
+      email: 'jhondoe@example.com',
+      password: '123456',
+    })
+
+    expect(response.statusCode).toEqual(200)
+    expect(response.body).toEqual({
+      token: expect.any(String),
+    })
+  })
+})
