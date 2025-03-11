e8d1c45f24370e21a07c987c402642cba81f6856
luigikk <luigimee15@gmail.com>
2025-03-11 18:35:54 -0300

    ⚙️fix: validade test

--- src/http/controllers/check-ins/validate.spec.ts
+++ src/http/controllers/check-ins/validate.spec.ts
@@ -1,8 +1,8 @@
 import request from 'supertest'
 import { afterAll, beforeAll, describe, expect, it, test } from 'vitest'
 import { app } from '../../../app'
-import { CreateAndAuthenticateUser } from '../../../utils/test/create-and-authenticate-user'
 import { prisma } from '../../../lib/prisma'
+import { CreateAndAuthenticateUser } from '../../../utils/test/create-and-authenticate-user'
 
 describe('Validate Check-in Controller (e2e)', () => {
   beforeAll(async () => {
@@ -13,8 +13,8 @@ describe('Validate Check-in Controller (e2e)', () => {
     await app.close()
   })
   it('should be able to validate a check-in', async () => {
-    const { token } = await CreateAndAuthenticateUser(app)
-    
+    const { token } = await CreateAndAuthenticateUser(app, true)
+
     const user = await prisma.user.findFirstOrThrow()
     const gym = await prisma.gym.create({
       data: {
@@ -23,14 +23,14 @@ describe('Validate Check-in Controller (e2e)', () => {
         phone: null,
         latitude: 12.7429572,
         longitude: -38.2112697,
-      }
+      },
     })
-    
+
     let checkIn = await prisma.checkIn.create({
       data: {
-          gym_id: gym.id,
-          user_id: user.id,
-      }
+        gym_id: gym.id,
+        user_id: user.id,
+      },
     })
 
     const response = await request(app.server)
@@ -39,11 +39,11 @@ describe('Validate Check-in Controller (e2e)', () => {
       .send()
 
     expect(response.statusCode).toEqual(204)
-    
+
     checkIn = await prisma.checkIn.findUniqueOrThrow({
       where: {
-        id: checkIn.id
-      }
+        id: checkIn.id,
+      },
     })
     expect(checkIn.validated_at).toEqual(expect.any(Date))
   })
--- src/utils/test/create-and-authenticate-user.ts
+++ src/utils/test/create-and-authenticate-user.ts
@@ -1,11 +1,19 @@
+import { hash } from 'bcryptjs'
 import type { FastifyInstance } from 'fastify'
 import request from 'supertest'
+import { prisma } from '../../lib/prisma'
 
-export async function CreateAndAuthenticateUser(app: FastifyInstance) {
-  await request(app.server).post('/users').send({
-    name: 'Jhon Doe',
-    email: 'jhondoe@example.com',
-    password: '123456',
+export async function CreateAndAuthenticateUser(
+  app: FastifyInstance,
+  isAdmin = false
+) {
+  const user = await prisma.user.create({
+    data: {
+      name: 'Jhon Doe',
+      email: 'jhondoe@example.com',
+      password_hash: await hash('123456', 6),
+      role: isAdmin ? 'ADMIN' : 'MEMBER',
+    },
   })
   const authResponse = await request(app.server).post('/sessions').send({
     email: 'jhondoe@example.com',
